113cb89b3571b0000abfc53e99f76b46fde167b0
Eduardo da Silva Brito <eduardob.silva0606@gmail.com>
2025-08-29 12:15:12 -0300

    create forms

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,8 @@
+<?php
+<?php
+$nome = $_POST['nome'] ?? '';
+$sobrenome = $_POST['sobrenome'] ?? '';
+echo "Cadastro realizado com sucesso!<br>";
+echo "Nome: $nome<br>";
+echo "Sobrenome: $sobrenome<br>";
+?>
