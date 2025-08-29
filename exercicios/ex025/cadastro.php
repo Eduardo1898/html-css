33fd5c8ff969edc2366afd18a69cd0d3a8d55d8d
Eduardo da Silva Brito <eduardob.silva0606@gmail.com>
2025-08-29 16:48:10 -0300

    delete cadastro.php

--- exercicios/ex025/cadastro.php
+++
@@ -1,8 +0,0 @@
-<?php
-<?php
-$nome = $_POST['nome'] ?? '';
-$sobrenome = $_POST['sobrenome'] ?? '';
-echo "Cadastro realizado com sucesso!<br>";
-echo "Nome: $nome<br>";
-echo "Sobrenome: $sobrenome<br>";
-?>
